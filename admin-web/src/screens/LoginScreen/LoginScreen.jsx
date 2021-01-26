6d9c61d975d360bddeea0bcf18ffe889e46b57be
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2021-01-26 21:14:29 +0100

    Push

---
+++ admin-web/src/screens/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './Loginscreen.css'
+
+export default class LoginScreen extends Component{
+    constructor(){
+        super();
+        this.state = {}
+    }
+    render(){
+        return(
+            <div className='Login-container'>
+                Hello
+            </div>
+        )
+    }
+}
