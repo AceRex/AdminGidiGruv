c2a9d5616c4174b2a42561b5fdf62673955b914f
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2021-02-03 11:22:54 +0100

    PushNow

--- admin-web/src/compoments/header.jsx
+++ admin-web/src/compoments/header.jsx
@@ -55,10 +55,10 @@ export default class Header extends Component {
                         <ListOfEvents/>
                     </TabPanel>
                     <TabPanel>
-                        <h2>Any content 3</h2>
+                        
                     </TabPanel>
                     <TabPanel>
-                        <h2>Any content 4</h2>
+                        
                     </TabPanel>
                 </Tabs>
             </>
